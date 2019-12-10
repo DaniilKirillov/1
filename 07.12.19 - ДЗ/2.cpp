0a889b562c9a8d02dc563551959f10893491bed7
DaniilKirillov <57501470+DaniilKirillov@users.noreply.github.com>
2019-12-10 19:13:41 +0300

    Add files via upload

---
+++ 07.12.19 - ДЗ/2.cpp
@@ -0,0 +1,44 @@
+﻿// Напишите программу на основе разделяй и властвуй позволяющую эффективно проверять принадлежность пары значений массиву.Например, пара(2, 3) принадлежит массиву[1, 2, 3, 5, 7, 11, 13, 17], а пара(3, 4) - нет.Можно считать, что количество запросов многократно превышает размер массива.В комментариях напишите, почему перебор - эффективное
+
+
+#include <iostream>
+using namespace std;
+
+//Поиск элементов в массиве с помощью бинарного поиска:
+
+int Bin_search(int *arr, int left, int right, int key) {
+	int mid = 0;
+	int search = -1;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (key == arr[mid]) {
+			search = mid;
+			break;           
+		}
+		if (key < arr[mid]) {
+			right = mid - 1;
+		}
+		else if (key > arr[mid]) {
+			left = mid + 1;
+		}
+	}
+	if (search == -1) return -1;
+	else return search;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	int* arr;
+	arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	int a, b;
+	cin >> a >> b;
+	if (Bin_search(arr, 0, n, a) == -1 || Bin_search(arr, 0, n, b) == -1) {
+		cout << "NO";
+	}
+	else cout << "YES";
+	return 0;
+}
