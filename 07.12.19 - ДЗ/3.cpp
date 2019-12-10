ddd2fb2f5f4c32b44164af580bd30adec48fd608
DaniilKirillov <57501470+DaniilKirillov@users.noreply.github.com>
2019-12-10 18:18:49 +0300

    Add files via upload

---
+++ 07.12.19 - ДЗ/3.cpp
@@ -0,0 +1,18 @@
+﻿// Для чисел Фибоначчи известны некоторые тождества:
+//Докажите эти тождества и напишите программу для вычисления ЧФ с помощью разделяй и властвуй.
+
+
+#include <iostream>
+using namespace std;
+
+int Fib(int n) {
+	if (n < 3) return 1;
+	else return Fib(n - 1) + Fib(n - 2);
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << Fib(n);
+	return 0;
+}
